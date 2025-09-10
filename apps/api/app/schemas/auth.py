0587e24c036dcf1dd2ce6500f4129b4db09eec4a
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-10 12:20:28 +0300

    Create auth.py

---
+++ apps/api/app/schemas/auth.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+
+class LoginIn(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenOut(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class MeOut(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
