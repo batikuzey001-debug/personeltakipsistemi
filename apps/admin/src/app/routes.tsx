f1763f7d0179556bcd439466724df9441ccb34f6
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-10 10:28:16 +0300

    Create routes.tsx

---
+++ apps/admin/src/app/routes.tsx
@@ -0,0 +1,7 @@
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+
+export const routes = [
+  { path: "/login", element: <Login /> },
+  { path: "/dashboard", element: <Dashboard /> },
+];
