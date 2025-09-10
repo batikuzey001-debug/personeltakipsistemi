4b2f54d800a1d43e667af43a2d0e9807c0ea89a4
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-10 10:30:28 +0300

    Create main.tsx

---
+++ apps/admin/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./lib/auth";
+
+const el = document.getElementById("root")!;
+createRoot(el).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
