30faa1a104dfccd3cb834a7b90508b9247f00205
batikuzey001-debug <batikuzey001@gmail.com>
2025-10-26 14:12:18 +0300

    Update routes.tsx

--- apps/admin/src/app/routes.tsx
+++ apps/admin/src/app/routes.tsx
@@ -1,7 +1,11 @@
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import Reports from "../pages/Reports";
+import Personel from "../pages/Personel";
 
 export const routes = [
   { path: "/login", element: <Login /> },
   { path: "/dashboard", element: <Dashboard /> },
+  { path: "/reports", element: <Reports /> },
+  { path: "/personel", element: <Personel /> },
 ];
